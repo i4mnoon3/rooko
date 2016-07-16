3d3a8634d18fa6cf4ef27902c185420e0ce0f8a6
Ian Escarro <ian.escarro@gmail.com>
2016-07-16 10:07:48 +0800

    more update

--- src/Rooko.Tests/MigrationTests.cs
+++ src/Rooko.Tests/MigrationTests.cs
@@ -1,10 +1,12 @@
-﻿//	<file>
-//		<license></license>
-//		<owner name="Ian Escarro" email="ian.escarro@gmail.com"/>
-//	</file>
-
+﻿/*
+ * Created by SharpDevelop.
+ * User: Dev
+ * Date: 7/16/2016
+ * Time: 9:27 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
 using System;
-using System.Reflection;
 using NUnit.Framework;
 using Rooko.Core;
 
@@ -14,21 +16,7 @@ namespace Rooko.Tests
 	public class MigrationTests
 	{
 		[Test]
-		public void TestMigrate()
-		{
-			Migrator m = new Migrator(Assembly.LoadFile(@"C:\ian\projects\nbooks\trunk\bin\NBooks.Core.dll"), new MySQLMigrationRepository());
-			m.Migrate();
-		}
-		
-		[Test]
-		public void TestRollback()
-		{
-			Migrator m = new Migrator(Assembly.LoadFile(@"C:\ian\projects\nbooks\trunk\bin\NBooks.Core.dll"), new MySQLMigrationRepository());
-			m.Rollback();
-		}
-		
-		[Test]
-		public void a()
+		public void TestCreateItemsMigrate()
 		{
 			var m = new CreateItems();
 			m.Repository = new MySQLMigrationRepository();
@@ -36,7 +24,7 @@ namespace Rooko.Tests
 		}
 		
 		[Test]
-		public void b()
+		public void TestCreateItemsRollback()
 		{
 			var m =  new CreateItems();
 			m.Repository = new MySQLMigrationRepository();
@@ -55,13 +43,34 @@ namespace Rooko.Tests
 			CreateTable(
 				"items",
 				new Column("id", "integer", true, true, true),
-				new Column("name")
+				new Column("name"),
+				new Column("description"),
+				new Column("cost", "double")
 			);
 		}
 		
 		public override void Rollback()
 		{
 			DropTable("items");
+		}
+	}
+	
+	public class AddPriceToItems : Migration
+	{
+		public AddPriceToItems() : base("DA8965AD-31E3-4085-8125-8396758C4A82")
+		{
+		}
+		
+		public override void Migrate()
+		{
+			base.Migrate();
+			AddColumn("items", new Column("price", "double"));
+		}
+		
+		public override void Rollback()
+		{
+			base.Rollback();
+			RemoveColumn("items", "price");
 		}
 	}
 }
