e4d08107e623abe107e78487eccc82d6f49437c4
Ian Escarro <ian.escarro@gmail.com>
2016-07-29 10:11:26 +0800

    updated pack and push batch file

--- src/Rooko.Core/packnpush.bat
+++ src/Rooko.Core/packnpush.bat
@@ -1,3 +1,4 @@
-nuget pack Rooko.Core.csproj
-:: nuget setApiKey 558b8342-f274-48cd-b41e-5db35171df31 -Source https://www.nuget.org/api/v2/package
-nuget push %1 -Source https://www.nuget.org/api/v2/package
+:: Example: packnpush Rooko.Core 0.10
+nuget pack %1.csproj
+nuget setApiKey 558b8342-f274-48cd-b41e-5db35171df31 -Source https://www.nuget.org/api/v2/package
+nuget push %1.dll.%2.nupkg -Source https://www.nuget.org/api/v2/package
