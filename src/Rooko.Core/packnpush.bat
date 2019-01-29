9c2507a8c56a320110f8ad927586721c828aae4f
Ian Escarro <ian.escarro@gmail.com>
2019-01-29 19:58:03 +0800

    Added api key to environment variable

--- src/Rooko.Core/packnpush.bat
+++ src/Rooko.Core/packnpush.bat
@@ -1,3 +1,4 @@
 :: Example: packnpush Rooko.Core 0.10
 nuget pack %1.csproj
+nuget setApiKey %NUGET_API_KEY% -Source https://www.nuget.org/api/v2/package
 nuget push %1.dll.%2.nupkg -Source https://www.nuget.org/api/v2/package
